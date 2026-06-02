e0cabf439e82d25f3a1a1fc276b4d411d905a429
Marcelo Rodriguez <mar.rp89@gmail.com>
2026-06-02 02:23:58 -0400

    actualizo el CORS para produccion

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://weather-app-jade-six-59.vercel.app/"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
